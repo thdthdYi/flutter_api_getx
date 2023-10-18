08ed92ebdb102612c13d5ce8f62b3b710965b7ad
heesong_ju <songyn111@gmail.com>
2023-10-18 16:09:47 +0900

    완료

---
+++ lib/product/view/product_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
